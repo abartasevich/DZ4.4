4a5ff66ee2c885a70b9dd232752aada424a1115d
Andrei <abartasevich@vtb.ru>
2020-12-13 15:43:18 +0300

    Initial commit

---
+++ src/vtb/abartasevich/Main.java
@@ -0,0 +1,11 @@
+package vtb.abartasevich;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++)
+            System.out.print(i + " ");
+
+        System.out.println(" ");
+    }
+}
